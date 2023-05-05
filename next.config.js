ef078721cf7490365adceca7d0c60783fd15a93f
Ravi Dhanju <ravidhanju@Ravis-MacBook-Pro.local>
2023-05-05 10:00:00 -0400

    airbnb clone

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  images: {
+    domains: ["links.papareact.com", "www.jsonkeeper.com"],
+  },
+
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoicmF2aWRoYW5qdSIsImEiOiJjbGh6ZG5rb20xNTlvM29zMmE5ZjFueDdsIn0.YheAdtvOt1TO80gBN-GXeA",
+  },
+};
